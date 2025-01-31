eaf60500da290bc94ca908e898171c6aa3200f12
xddinsd <44743326+xddinsd@users.noreply.github.com>
2025-01-31 08:16:25 +0300

    fixed bug with installation of color-indent

--- lua/plugins/color-indent.lua
+++ lua/plugins/color-indent.lua
@@ -2,6 +2,8 @@
 
 return {
     'lukas-reineke/indent-blankline.nvim',
+    main   = "ibl",
+    commit = "29be0919b91fb59eca9e90690d76014233392bef", -- Lazy.nvim bug with v3 version. https://github.com/lukas-reineke/indent-blankline.nvim/discussions/692
     config = function()
         require('ibl').setup {
 
